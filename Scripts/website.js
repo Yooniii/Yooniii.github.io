50001fbd8b7e1fb506ccc4fd0240beeb91af60a2
Sarah Yoon <sarahyoon705@gmail.com>
2023-02-17 19:57:43 -0500

    Added new files

---
+++ Scripts/website.js
@@ -0,0 +1,19 @@
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
