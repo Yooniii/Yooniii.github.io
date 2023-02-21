c6c3049bb177631c76de7b5cd38949b5ceaf1a42
Sarah Yoon <sarahyoon705@gmail.com>
2023-02-20 23:27:55 -0500

    Fixed links

--- Scripts/website.js
+++ Scripts/website.js
@@ -1,19 +1,6 @@
-const scrollUp = document.querySelector("#scroll-up");
-
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
-
- document.querySelectorAll('a[href^="#about"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+document.querySelector('a[href="#about"]').addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector('#about').scrollIntoView({
+      behavior: "smooth"
     });
   });
